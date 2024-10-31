7dffb2394957331623762238235b8dfa235c9c0b
sean <ngs1214@gmail.com>
2024-10-31 16:17:11 +0900

    register clientSecret 폼추가

--- src/main/java/com/example/spring_oauth_authorization_server/dto/RegisterDTO.java
+++ src/main/java/com/example/spring_oauth_authorization_server/dto/RegisterDTO.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Setter
 public class RegisterDTO {
     private String clientName;
+    private String clientSecret;
     private String redirectUris;
     private String postLogoutRedirectUris;
     private String scopes;
