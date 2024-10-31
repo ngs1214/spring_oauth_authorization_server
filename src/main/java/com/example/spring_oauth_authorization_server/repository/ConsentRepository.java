5224eb8831edd30b5a9f3db30a09fc7e2ff26a93
sean <ngs1214@gmail.com>
2024-10-31 16:01:55 +0900

    코드 발급부 제3자 사용 동의 관련 저장

---
+++ src/main/java/com/example/spring_oauth_authorization_server/repository/ConsentRepository.java
@@ -0,0 +1,11 @@
+package com.example.spring_oauth_authorization_server.repository;
+
+import com.example.spring_oauth_authorization_server.entity.ConsentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ConsentRepository extends JpaRepository<ConsentEntity, String> {
+    Optional<ConsentEntity> findByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+    void deleteByRegisteredClientIdAndPrincipalName(String registeredClientId, String principalName);
+}
